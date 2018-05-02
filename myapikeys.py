3471a1ccdbc44f080f61899aeff0b6265a12fdb8
fay <wushiney@yahoo.com>
2018-05-02 19:16:42 +0000

    lecture 9

---
+++ myapikeys.py
@@ -0,0 +1,5 @@
+apikeys = {
+        'yelp': 'CIFUI8CGrZVeGXoE1Acji1mdXYlsI5bBDfOdBlIsreWVvNDJCY-deXFHIFtQlOSefAmSe8m0j6FZzozVYYoxDLTibjoEuVYSMNY4kBaKss56bfy13H7R0m0epmDfWnYx',
+        'quandl': 'bdXfbQpeRRcFQmzjmQgu',
+        'fred': 'db8722809b819223e7fe305918ef265c'
+}
